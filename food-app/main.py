414ba8cd18d33d39d23f9a1f205b72d7c9f74ece
irismanriquez <irisjmanriquez@student.hartnell.edu>
2017-08-10 23:07:48 -0700

    This should be the final outcome

--- food-app/main.py
+++ food-app/main.py
@@ -50,25 +50,15 @@ class SecondHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/secondpd.html')
         name = self.request.get("username")
         color = self.request.get("usercolor")
-        flavors_5 = self.request.get("flavors_5")
-        flavors_4 = self.request.get("flavors_4")
-        flavors_3 = self.request.get("flavors_3")
-        flavors_2 = self.request.get("flavors_2")
         flavors = self.request.get("flavors")
         places = self.request.get("places")
         portions = self.request.get("portions")
 
-        flavors_list = []
-        if flavors !="":
-            flavors_list.append(flavors)
-        if flavors_2 !="":
-            flavors_list.append(flavors_2)
-        if flavors_3 !="":
-            flavors_list.append(flavors_3)
-        if flavors_4 !="":
-            flavors_list.append(flavors_4)
-        if flavors_5 !="":
-            flavors_list.append(flavors_5)
+        flavors_list = [ str(self.request.get("flavors")), str(self.request.get("flavors_2")), str(self.request.get("flavors_3")), str(self.request.get("flavors_4")), str(self.request.get("flavors_5"))]
+
+        for flavor in flavors_list:
+            if flavor is not "":
+                typeFood = flavor
 
         #for flavor in flavors_list:
         if len(flavors_list) == 0:
@@ -76,14 +66,19 @@ class SecondHandler(webapp2.RequestHandler):
         elif len(flavors_list) == 1:
             flavors_string = "You have chosen {0}.<br>".format(flavors_list[0])
         else:
-            base_flavor_string = "You have chosen the following flavors: "
+            base_flavor_string = "Based on your selection, we recommend this food: "
             flavors_string = ""
             for flavors in flavors_list:
                 flavors_string += flavors + ", "
             flavors_string = base_flavor_string + flavors_string[0:-2] + ".<br>"
         #self.response.write("Hello " + name + " you have chosen " + flavors + " and based on your location we have found this place near you, " + places + ", and the amount of your portion is " + portions + ".")
         logging.info(flavors_list)
-        self.response.write(template.render())
+
+        context = {
+            'typeFood': typeFood
+        }
+
+        self.response.write(template.render(context))
         self.response.write("<br><br>"+flavors_string)
         self.response.write("Your name is: <strong> " + name + "</strong> Your favorite color is: <strong> " + color + "</strong> <br>")
 
